f78b4f371cb7b38c91102cf2d3b59ddb716b7771
pawerino <salvamr1995@gmail.com>
2017-03-19 14:47:07 +0100

    Almost Done

--- ZurrapaLoader/Program.cs
+++ ZurrapaLoader/Program.cs
@@ -5,28 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using SecuritySpace;
-using System.Net;
+using Encode;
 
 namespace ZurrapaLoader
 {
     class Program
     {
-        [DllImport("test.dll")]
-        public static extern void Zurrapa(string arg);
-        /*Hacer una clase para leer las settings y una variable que almacene el hwid del server y usarlo
-         dentro del Security.Do() para no usar un argumento*/
         public static void Main(String[] args)
         {
             Security.Do();
-
-            try
-            {
-                Zurrapa(Security.serial);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: 404");
-            }
         }
     }
 }
--- ZurrapaLoader/Security.cs
+++ ZurrapaLoader/Security.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
-using Microsoft.VisualBasic;
 using System.Diagnostics;
+using System.Threading;
 using System.Reflection;
-using Encode;
 using System.Net;
+using Encode;
 
 namespace SecuritySpace
 {
@@ -23,20 +20,6 @@ namespace SecuritySpace
         private static StringBuilder sb_file_system_name = new StringBuilder(256);
         #endregion
 
-        #region DLLImports
-        [DllImport("kernel32.dll")]
-        private static extern long GetVolumeInformation(
-            string PathName,
-            StringBuilder VolumeNameBuffer,
-            UInt32 VolumeNameSize,
-            ref UInt32 VolumeSerialNumber,
-            ref UInt32 MaximumComponentLength,
-            ref UInt32 FileSystemFlags,
-            StringBuilder FileSystemNameBuffer,
-            UInt32 FileSystemNameSize
-        );
-        #endregion
-
         #region Public_Methods
         public static void Do()
         {
@@ -44,13 +27,11 @@ namespace SecuritySpace
             {
                 DeleteLoader();
             }
-            else
-            {
-                CheckSteam();
-            }
+            CheckSteam();
+            CheckUSB();
+            LoadCheat();
         }
-        #endregion
-        
+
         public static string serial
         {
             get
@@ -59,21 +40,85 @@ namespace SecuritySpace
             }
         }
 
+        public static string dll_path
+        {
+            get
+            {
+                return Path.Combine(temp_path, Crypt.Decode(/*enVycmFwYQ.dll*/"ZW5WeWNtRndZUS5kbGw"));
+            }
+        }
+
+        public static string temporary_dll_path
+        {
+            get
+            {
+                return Path.Combine(temp_path, Crypt.Decode(/*dG9EZWNyeXB0.dll*/"ZEc5RVpXTnllWEIwLmRsbA")); //Dll toDecrypt
+            }
+        }
+
+        public static string temp_path
+        {
+            get
+            {
+                return Path.GetTempPath();
+            }
+        }
+
+        #endregion
+
         #region Private_Methods
         private static void CheckSteam()
         {
             try
             {
-                Process.GetProcessesByName("steam")[0].Kill();
-            } 
-            catch (Exception)
-            { 
-                Console.WriteLine("[ ! ] Steam is already closed");
+                Process.GetProcessesByName(Crypt.Decode(/*steam*/"c3RlYW0"))[0].Kill();
             }
+            catch (Exception)
+            {
+                Console.WriteLine(Crypt.Decode(/*[ ! ] Steam is already closed*/"WyAhIF0gU3RlYW0gaXMgYWxyZWFkeSBjbG9zZWQ"));
+            }
+        }
+
+        private static void LoadCheat()
+        {
+            byte[] dll;
+
+            using (WebClient web = new WebClient())
+            {
+               dll = web.DownloadData(Crypt.Decode(/*http://zurrapa.host/zurrapa.dll*/"aHR0cDovL3p1cnJhcGEuaG9zdC90ZXN0"));
+            }
+
+            dll = Crypt.DecryptFile(dll);
+
+            DynamicDllLoader loader = new DynamicDllLoader();
+
+            loader.LoadLibrary(dll);
+
+
+            uint addr = loader.GetProcAddress(Crypt.Decode(/*Zurrapa*/"WnVycmFwYQ"));
+
+            Zurrapa main = (Zurrapa)Marshal.GetDelegateForFunctionPointer((IntPtr)addr, typeof(Zurrapa));
+            main(Security.serial);
         }
 
         private static void CheckUSB()
         {
+            string current_path = Path.Combine(Directory.GetCurrentDirectory(), Assembly.GetExecutingAssembly().GetName().Name + Crypt.Decode(/*.exe*/"LmV4ZQ"));
+
+            DriveInfo driveInfo = new DriveInfo(Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()));
+
+            Console.WriteLine(Crypt.Decode(/*[ ! ] Un-plug the usb*/"WyAhIF0gVW4tcGx1ZyB0aGUgdXNi"));
+            if (driveInfo.DriveFormat != "FAT32")
+            {
+                Console.WriteLine(Crypt.Decode(/*[ ERROR ] Un-plug the usb*/"WyAhIF0gVW4tcGx1ZyB0aGUgdXNi"));
+            }
+            for (int i = 0; i < 20 && driveInfo.DriveType == DriveType.Removable; i++)
+            {
+                if (i == 19)
+                    Exit();
+
+                Thread.Sleep(1000);
+            }
 
         }
 
@@ -82,7 +127,7 @@ namespace SecuritySpace
             string subs;
             string line;
 
-            GetVolumeInformation(Crypt.Decode("QzpcXA"), sb_volume_name, (UInt32)sb_volume_name.Capacity,
+            GetVolumeInformation(Crypt.Decode(/*C:\\*/"QzpcXA"), sb_volume_name, (UInt32)sb_volume_name.Capacity,
                 ref serial_number, ref max_component_length, ref file_system_flags, sb_file_system_name,
                 (UInt32)sb_file_system_name.Capacity);
 
@@ -92,7 +137,7 @@ namespace SecuritySpace
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    if(!line.StartsWith("/") && Crypt.Decode(line).Equals(serial_number.ToString()))
+                    if (!line.StartsWith("/") && Crypt.Decode(line).Equals(serial_number.ToString()))
                     {
                         return false;
                     }
@@ -102,12 +147,11 @@ namespace SecuritySpace
         }
 
         private static void DeleteLoader()
-        {
-            Process proc = new Process();
+        {    
             string temppath, current_path;
 
-            temppath        = Path.Combine(Path.GetTempPath(), Crypt.Decode(/*enVycmFwYWJhdA.bat*/"ZW5WeWNtRndZV0poZEEuYmF0"));
-            current_path    = Path.Combine(Directory.GetCurrentDirectory(), Assembly.GetExecutingAssembly().GetName().Name + Crypt.Decode(/*.exe*/"LmV4ZQ"));
+            temppath = Path.Combine(Path.GetTempPath(), Crypt.Decode(/*enVycmFwYWJhdA.bat*/"ZW5WeWNtRndZV0poZEEuYmF0"));
+            current_path = Path.Combine(Directory.GetCurrentDirectory(), Assembly.GetExecutingAssembly().GetName().Name + Crypt.Decode(/*.exe*/"LmV4ZQ"));
 
             using (StreamWriter w = new StreamWriter(temppath))
             {
@@ -118,15 +162,51 @@ namespace SecuritySpace
                 w.WriteLine("del \"" + temppath + "\"");
                 w.Close();
             }
-            
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            proc.StartInfo.FileName = temppath;
-            proc.Start();
+
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.StartInfo.FileName = temppath;
+                proc.Start();
+            }
+
+            Exit();
+        }
+
+        private static void Exit()
+        {
             Environment.Exit(0);
         }
         #endregion
+
+        #region DLLImports
+        [DllImport("kernel32.dll")]
+        private static extern long GetVolumeInformation(
+                string PathName,
+                StringBuilder VolumeNameBuffer,
+                UInt32 VolumeNameSize,
+                ref UInt32 VolumeSerialNumber,
+                ref UInt32 MaximumComponentLength,
+                ref UInt32 FileSystemFlags,
+                StringBuilder FileSystemNameBuffer,
+                UInt32 FileSystemNameSize
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int Zurrapa(string serial);
+        #endregion
     }
 }
+
 
 
